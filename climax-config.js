8071f561b6df1e8ab9ce9444fc2996c2676ce3fd
Benardssuubi1 <bensuubi614@gmail.com>
2026-07-07 12:07:52 +0300

    Update API_KEY in climax-config.js

--- climax-config.js
+++ climax-config.js
@@ -1,6 +1,4 @@
 window.CLIMAX_CONFIG = {
-  // URL is safe to be here as it's public
   API_URL: 'https://flexiorder-api.onrender.com',
-
-  API_KEY: window.ENV_API_KEY || null 
+  API_KEY: '@6Foodies' 
 };
