4aeb506b3e53cbc8a779e38a60de985f141ef29d
Benardssuubi1 <bensuubi614@gmail.com>
2026-07-10 16:07:05 +0300

    Delete climax-config.js

--- climax-config.js
+++
@@ -1,6 +0,0 @@
-// climax-config.js
-window.CLIMAX_CONFIG = {
-  API_URL: 'https://flexiorder-api.onrender.com',
-  API_KEY: '@6Foodies' 
-};
-console.log("Config loaded successfully:", window.CLIMAX_CONFIG);
